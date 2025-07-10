877fcb17953082993d9fd94dd417948e2b882bb6
Romina Martinez <romina.martinez496@gmail.com>
2025-07-10 20:49:25 -0300

    Primer commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <footer>
+      <p>Sitio desarrollado por Romi Martínez </p>
+    </footer>
+    )
+}
+
+export default Footer;
